98fd5b0c49af3a6f2616f56300c7f8b67612f3ce
ghosttravislt <enochbaafi56@gmail.com>
2024-07-29 14:47:48 +0100

    portfolio website

---
+++ main.js
@@ -0,0 +1,13 @@
+let  menu = document.querySelector(".menu");
+let  close = document.querySelector(".close");
+
+
+
+menu.addEventListener("click", () => {
+   let navmenu =document.querySelector(".navdiv")
+    navmenu.classList.toggle("active")
+     console.log("hello")
+    
+})
+
+
